13893e962b2d141af926935350040dcdd1067545
Adam Herstein <adam.herstein@kroger.com>
2020-06-18 21:05:40 -0500

    Change to typescript and write plugin

---
+++ src/temperature.ts
@@ -0,0 +1,66 @@
+import {
+  AccessoryConfig,
+  AccessoryPlugin,
+  API,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  HAP,
+  Logging,
+  Service,
+} from 'homebridge'
+// @ts-ignore
+import { sensor } from 'node-dht-sensor'
+
+let hap: HAP
+
+export = (api: API) => {
+  hap = api.hap
+  api.registerAccessory('Temperature', Temperature)
+}
+
+class Temperature implements AccessoryPlugin {
+  private readonly log: Logging
+  private readonly name: string
+  private readonly scale: string
+
+  private readonly temperatureService: Service
+  private readonly informationService: Service
+
+  constructor(log: Logging, config: AccessoryConfig, api: API) {
+    this.log = log
+    this.name = config.name
+    this.scale = config.scale || 'c'
+
+    this.temperatureService = new hap.Service.Switch(this.name)
+
+    this.temperatureService
+      .getCharacteristic(hap.Characteristic.On)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        sensor.read(11, 4, (err: Error, temperature: number, humidity: number) => {
+          if (!err) {
+            const temperature_f = (9 / 5) * temperature + 32
+            log.info(`temp: ${temperature}°C (${temperature_f}°F), humidity: ${humidity}%`)
+            callback(undefined, this.scale === 'f' ? temperature_f : temperature)
+          }
+        })
+      })
+
+    this.informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, 'Custom Manufacturer')
+      .setCharacteristic(hap.Characteristic.Model, 'DHT11')
+
+    log.info('Temperature sensor finished initializing!')
+  }
+
+  identify(): void {
+    this.log('Temperature')
+  }
+
+  /*
+   * This method is called directly after creation of this instance.
+   * It should return all services which should be added to the accessory.
+   */
+  getServices(): Service[] {
+    return [this.informationService, this.temperatureService]
+  }
+}
--- temperature.js
+++
@@ -1,8 +0,0 @@
-const sensor = require('node-dht-sensor')
-
-sensor.read(11, 4, function (err, temperature, humidity) {
-  if (!err) {
-    const temperature_f = (9 / 5) * temperature + 32
-    console.log(`temp: ${temperature}°C (${temperature_f}°F), humidity: ${humidity}%`)
-  }
-})
